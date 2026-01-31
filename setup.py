6beb31e0cc9b17a453dec0049e32b1d4fab873aa
Alex Sigman <alex@aiva.ai>
2026-01-31 19:33:06 +0100

    setup.py modifications

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name="music-data-pipeline",
     version="0.1",
     author="atsigman",
-    packages=find_packages(),
+    packages=find_packages(where="src)",
+    package_dir={"": "src"},
     install_requires=requirements,
 )
